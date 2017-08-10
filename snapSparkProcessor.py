82e0f93e4d7eacf85f27a76e519580216f94eb66
VulcanoAhab <vulcano.ahab@gmail.com>
2017-08-09 23:37:18 -0300

    Adding pySpark processor

--- snapSparkProcessor.py
+++ snapSparkProcessor.py
@@ -1,28 +1,122 @@
+
 import re
-from pyspark.sql.functions import udf
+import string
+import datetime
+import tldextract
+from lxml import html
+
+
+from operator import add
+from pyspark.sql import Row
+from pyspark.sql import SQLContext
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf
+from pyspark.sql.types import StringType, TimestampTime
+
+# == udf[s]
 
 
+
+# == process class
 class Basic:
     """
+    Need to implement transformations
+    on scala udf to be called from python
     """
     _appName="wayBackeess"
+
     _spark=SparkSession.builder\
         .appName(_appName)\
         .getOrCreate()
 
-    def __init__(self, pageIn):
+    _wayLink=re.compile("(?:http://web.archive.org)?/web/\d+(?:im_)?/", re.I)
+
+    _frameRow=Row(
+        "raw",
+        "urls",
+        "words",
+        "title",
+        "website"
+        "created_at",
+        "captured_at"
+    )
+
+    _urlsRow=Row("url","domain")
+
+    @classmethod
+    def _getDomain(cls, urlIn):
         """
         """
+        _exResult=cls._tdlex(urlIn)
+        return _exResult.registered_domain
+
+    @classmethod
+    def toUrlObj(cls, urlIn):
+        """
+        """
+        urlClean=cls._wayLink.sub("",urlIn)
+        domain=cls._getDomain(urlClean)
+        return self._urlsRow(url=urlClean, domain=domain)
+
+    def __init__(self, pageIn, website, created_at):
+        """
+        """
+
         self._raw=pageIn
+        self._website=webiste
+        self._created=created_at
+
         self._rdd=self._spark.sparkContext.parallelize([pageIn])
+        self._rdd.cache()
+        self._df=None
 
-    @property
-    def rdd(self):
+    def findUrls(self, textIn):
         """
         """
-        return self._rdd.collect()
+        _xis=html.fromstring(textIn)
 
+        _elsTarget={
+            "href":"//@href",
+            "src":"//@src",
+            "data-src":"//@data-src"
+        }
+        _filterNot={
+            lambda x:not x.startswith("/static/"),
+        }
+
+        #build lists
+        _urls={}
+        for attr,xpas in _elsTarget.items():
+                #mine and filter urls
+                ustemp=_xis.xpath(xpas)
+                for condition in _filterNot:
+                    ustemp=filter(condition, ustemp)
+                #process urls
+                for utemp in ustemp:
+                    utemp=self.cleanUrl(utemp)
+                    if attr not in _urls:_urls.update({attr:[]})
+                    _urls[attr].append({
+                        "url":utemp,
+                        "domain":self._getDomain(utemp),
+                        })
+        return _urls
+
+    def processWords(self):
+        """
+        """
+        _words=self._rdd.flatMap(lambda x: x.split(" "))\
+                                .map(lambda x: (x,1))\
+                                .reduceByKey(add)
+        self._wordsCount=_words.collect()
+        print(self._wordsCount)
+
+    def processUrls(self):
+        """
+        """
+        _urls=self._rdd.flatMap(lambda x:self.findUrls(x))\
+                                    .map(lambda x:self.toUrlObj(x))
+        self._urls=_urls.collect()
+        print(self._urls)
 
     def close(self):
         """
